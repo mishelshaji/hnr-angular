d68c7c2e5fe250df93ea8ea7be067afdeb6c73a5
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2025-07-26 15:46:42 +0530

    Login component

--- src/app/common/components/button/button.component.ts
+++ src/app/common/components/button/button.component.ts
@@ -10,4 +10,25 @@ export class ButtonComponent {
   @Input() type: 'submit' | 'reset' | 'button' = 'button';
   @Input() disabled = false;
   @Input() text = 'Button';
+  @Input() severity: 'primary' | 'secondary' | 'danger' | 'success' = 'primary';
+
+  get classes() {
+    const cls = ['btn'];
+
+    switch (this.severity) {
+      case 'primary':
+        cls.push('btn-primary');
+        break;
+      case 'secondary':
+        cls.push('btn-secondary');
+        break;
+      case 'danger':
+        cls.push('btn-danger');
+        break;
+      case 'success':
+        cls.push('btn-success');
+        break;
+    }
+    return cls;
+  }
 }
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './features/login/login.component';
+import {ButtonComponent} from '../../common/components/button/button.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ButtonComponent
   ]
 })
 export class AuthModule { }
